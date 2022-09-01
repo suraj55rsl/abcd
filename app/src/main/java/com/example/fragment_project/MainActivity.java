00d7ea1c0ffe7c01bbb4143f15087d5798ea668e
Suraj Kumar <suraj.kumar@rsllearning.com>
2022-09-01 17:49:09 +0530

    changes are made

--- app/src/main/java/com/example/fragment_project/MainActivity.java
+++ app/src/main/java/com/example/fragment_project/MainActivity.java
@@ -1,14 +1,42 @@
 package com.example.fragment_project;
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(savedInstanceState==null)
+            getSupportFragmentManager().beginTransaction().add(R.id.frameForFragment,new HomeFragment()).commit();
+    }
+
+    public void showTime(View view) {
+        timeFragment tf=new timeFragment();
+        FragmentTransaction ft= getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.frameForFragment,tf);
+        ft.commit();
+        ft.addToBackStack(null);
+
+    }
+    public void showDate(View view) {
+        dateFragment tf=new dateFragment();
+        FragmentTransaction ft= getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.frameForFragment,tf);
+        ft.commit();
+        ft.addToBackStack(null);
+
+    }
+    public void showBattery(View view) {
+        batteryFragment tf=new batteryFragment();
+        FragmentTransaction ft= getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.frameForFragment,tf);
+        ft.commit();
+        ft.addToBackStack(null);
+
     }
 }
